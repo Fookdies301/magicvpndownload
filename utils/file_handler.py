33525d704e469219c837075e247ab345c2837950
nkhadka <nbnnbnkhadka@gmail.com>
2018-10-03 09:47:59 +0545

    Creating endpoint

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -7,18 +7,16 @@ def download_file(url, filename):
         Downloads file from the url and save it as filename
     """
     # check if file already exists
-    if os.path.isfile(filename):
-        os.remove(filename)
-    print('Downloading File')
-    response = requests.get(url)
-    # Check if the response is ok (200)
-    if response.status_code == 200:
-        # Open file and write the content
-        file_abs_path = os.path.join('downloads', filename)
-        with open(file_abs_path, 'wb') as file:
-            # A chunk of 128 bytes
-            for chunk in response:
-                file.write(chunk)
-    print(os.listdir('downloads'))
-    print('******')
-    print(os.system('pwd'))
+    if not os.path.isfile(filename):
+        print('Downloading File')
+        response = requests.get(url)
+        # Check if the response is ok (200)
+        if response.status_code == 200:
+            # Open file and write the content
+            with open(filename, 'wb') as file:
+                # A chunk of 128 bytes
+                for chunk in response:
+                    file.write(chunk)
+    else:
+        print('File exists')
+    print('Completed download in server')
