2919f701a1e41ee4605c771965c03a2ed184f741
nkhadka <nbnnbnkhadka@gmail.com>
2018-10-03 09:10:19 +0545

    Creating endpoint

--- views/frontend.py
+++ views/frontend.py
@@ -1,5 +1,4 @@
-import urllib
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from utils.file_handler import download_file as download
 
@@ -13,6 +12,7 @@ def index():
 
 @frontend.route('/download_file', methods=['GET', 'POST'])
 def download_file():
+    print(request.form.get('url_to_download'))
     url = 'https://www.dmepdac.com/docs/crosswalk/october_18/2018-10-05XWalkFinalVersion.xlsx'
     filename = url.rsplit('/', 1)[1]
     download(url, filename)
